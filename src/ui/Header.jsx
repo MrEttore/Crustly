d6f1d07517e32e7d644674c9e873b77113448a1e
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-03-21 10:15:51 +0100

    initial commit with first app implementations

---
+++ src/ui/Header.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <Link to="/">Crustly Co.</Link>
+      <p>Ettore</p>
+    </header>
+  );
+}
