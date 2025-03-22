a48025b705caf8a4cc0ce3ea11b18a461466b9e5
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-03-22 12:25:05 +0100

    add main app layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
-import Header from "./Header";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from 'react-router-dom';
+import CartOverview from '../features/cart/CartOverview';
+import Loader from './Loader';
+import Header from './Header';
 
 export default function AppLayout() {
+  // Get global app loading state with navigation hook.
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
   return (
     <div>
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
