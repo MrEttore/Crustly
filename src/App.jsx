54d4d87367157a28f6f0c8f838165cb9ba680a76
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-03-22 12:26:13 +0100

    add loader to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
-import AppLayout from "./ui/AppLayout";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './ui/Home';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+
+// Implement a "Render-As-You-Fetch" strategy in the router.
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     children: [
-      { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu /> },
-      { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      { path: '/', element: <Home /> },
+      { path: '/menu', element: <Menu />, loader: menuLoader }, // 2. Provide loader to path.
+      { path: '/cart', element: <Cart /> },
+      { path: '/order/new', element: <CreateOrder /> },
+      { path: '/order/:orderId', element: <Order /> },
     ],
   },
 ]);
