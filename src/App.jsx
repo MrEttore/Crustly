811882d2d216bf38bf8b0c67beb702081d7317f9
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-03-24 16:30:36 +0100

    refactor routing in App component to enhance error handling and data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './ui/Home';
-import Menu, { loader as menuLoader } from './features/menu/Menu';
-import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
-import Order from './features/order/Order';
 import AppLayout from './ui/AppLayout';
+import Home from './ui/Home';
+import Error from './ui/Error';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Order, { loader as orderLoader } from './features/order/Order';
+import CreateOrder, {
+    action as createOrderAction,
+} from './features/order/CreateOrder';
+import Cart from './features/cart/Cart';
 
 // Implement a "Render-As-You-Fetch" strategy in the router.
+
+// loaders -> fetch data for a route before rendering it (GET).
+// actions -> write data to a server before rendering a route (POST, PATCH, DELETE).
 
 const router = createBrowserRouter([
     {
         element: <AppLayout />,
+        errorElement: <Error />,
         children: [
             { path: '/', element: <Home /> },
-            { path: '/menu', element: <Menu />, loader: menuLoader }, // 2. Provide loader to path.
+            {
+                path: '/menu',
+                element: <Menu />,
+                errorElement: <Error />,
+                loader: menuLoader, // 2a. Provide loader to path.
+            },
             { path: '/cart', element: <Cart /> },
-            { path: '/order/new', element: <CreateOrder /> },
-            { path: '/order/:orderId', element: <Order /> },
+            {
+                path: '/order/new',
+                element: <CreateOrder />,
+                action: createOrderAction, // 2b. Provide action to path.
+            },
+            {
+                path: '/order/:orderId',
+                element: <Order />,
+                errorElement: <Error />,
+                loader: orderLoader,
+            },
         ],
     },
 ]);
