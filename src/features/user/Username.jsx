78bdce77d5705943772e863e4274b7393f552415
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-03-24 16:50:15 +0100

    refactor layout for improved responsiveness

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,3 +1,3 @@
 export default function Username() {
-    return <div className="hidden text-sm font-semibold">Ettore</div>;
+    return <div className="hidden text-sm font-semibold md:block">Ettore</div>;
 }
