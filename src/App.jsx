23c6f276b8ce4ab9c27f9a762e8a2cee850888f3
Ettore Marangon <Ettore.Marangon@mhp.com>
2025-04-16 16:06:26 +0200

    add updateOrder action to Order route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './ui/Home';
 import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Order, { loader as orderLoader } from './features/order/Order';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
 import CreateOrder, {
     action as createOrderAction,
 } from './features/order/CreateOrder';
@@ -37,6 +38,7 @@ const router = createBrowserRouter([
                 element: <Order />,
                 errorElement: <Error />,
                 loader: orderLoader,
+                action: updateOrderAction,
             },
         ],
     },
